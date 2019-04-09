ef842b969581774254e965162756a476ddc596f0
Gerald Spenlingwimmer <s1810454039@students.fh-hagenberg.at>
2019-04-09 17:32:34 +0200

    added dao implementation generator

--- RestGenerator/src/main/java/sve/free/ex/gs/generator/writer/DaoGenerator.java
+++ RestGenerator/src/main/java/sve/free/ex/gs/generator/writer/DaoGenerator.java
@@ -49,7 +49,56 @@ public class DaoGenerator {
     }
 
     private void generateDaoImplementation(GenerationConfig config) {
+        StringGenerator generator = new StringGenerator();
+        ContentWriter writer = new ContentWriter();
+        StringBuilder sb = new StringBuilder();
 
+        generator.addPackage(sb, config, "dao.implementation");
+
+        String lowerCaseStart = config.getResourceName().substring(0, 1).toLowerCase() + config.getResourceName().substring(1);
+
+        sb.append(
+                        "import "+ config.getBasePackage() + ".domain." + config.getResourceName() + ";\n" +
+                        "import javax.ejb.Stateless;\n" +
+                        "import javax.persistence.EntityManager;\n" +
+                        "import javax.persistence.PersistenceContext;\n" +
+                        "import javax.persistence.TypedQuery;\n" +
+                        "import java.util.List;\n" +
+                        "\n" +
+                        "@Stateless // V 1.0\n" +
+                        "public class " + config.getResourceName() +"DaoBean implements " + config.getResourceName() + "Dao {\n" +
+                        "\n" +
+                        "    @PersistenceContext(unitName = \"TODO:\") // inject entity manager\n" +
+                        "    private EntityManager em;\n" +
+                        "\n" +
+                        "    @Override\n" +
+                        "    public " + config.getResourceName() + " findById(Long id) {\n" +
+                        "        return em.find(" + config.getResourceName() + ".class, id);\n" +
+                        "    }\n" +
+                        "\n" +
+                        "    @Override\n" +
+                        "    public List<" + config.getResourceName() + "> findAll() {\n" +
+                        "        TypedQuery<" + config.getResourceName() + "> selQry = em.createQuery(\"select c from " + config.getResourceName() + " c\", " + config.getResourceName() + ".class);\n" +
+                        "        return selQry.getResultList();\n" +
+                        "    }\n" +
+                        "\n" +
+                        "    @Override\n" +
+                        "    public " + config.getResourceName() + " merge(" + config.getResourceName() + " " + lowerCaseStart + ") {\n" +
+                        "        return em.merge(" + lowerCaseStart + ");\n" +
+                        "    }\n" +
+                        "\n" +
+                        "    @Override\n" +
+                        "    public void persist(" + config.getResourceName() + " " + lowerCaseStart + ") {\n" +
+                        "        em.persist(" + lowerCaseStart + ");\n" +
+                        "    }\n" +
+                        "\n" +
+                        "    @Override\n" +
+                        "    public void remove(" + config.getResourceName() + " " + lowerCaseStart + ") {\n" +
+                        "        em.remove(" + lowerCaseStart + ");\n" +
+                        "    }\n" +
+                        "}\n");
+
+        writer.write(config.getPath() + "/dao/implementation", config.getResourceName() + "DaoBean", sb.toString());
     }
 
     public void generate(GenerationConfig config) {
